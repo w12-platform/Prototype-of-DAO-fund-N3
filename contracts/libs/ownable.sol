7c2e7c2e8c5b73d9b847d0370075426d43711533
Mike Korneev <korneevm@gmail.com>
2018-04-12 00:21:26 +0300

    Project set up, research of DAO contacts

---
+++ contracts/libs/ownable.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.16;
+
+contract ownable {
+    address public owner;
+
+    function ownable()  public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address newOwner) onlyOwner  public {
+        owner = newOwner;
+    }
+}
